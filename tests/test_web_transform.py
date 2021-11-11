4aba23356d4ca2a68e9e05e2a797e1fd9f3f77cd
IanDoarn <baxterhusky3@gmail.com>
2021-11-11 08:49:07 -0500

    Unit tests for pandas stuff in web_transform.py

---
+++ tests/test_web_transform.py
@@ -0,0 +1,49 @@
+import unittest
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+import src.web_transform
+
+
+class TestWebTransform(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.csv_file = Path('dummy.csv')
+        self.tf_config = {
+            'columns':
+                ['user_id', 'length', 'path'],
+            'index': 'user_id'
+        }
+
+    def test_tf_data_to_output_result_type(self):
+
+        result = src.web_transform.tf_data_to_output(
+            self.csv_file,
+            selected_columns=self.tf_config['columns'],
+            index=self.tf_config['index']
+        )
+
+        assert isinstance(result, pd.DataFrame)
+        assert isinstance(
+            result.columns,
+            pd.core.indexes.base.Index
+        )
+
+    def test_tf_data_to_output_result_columns(self):
+
+        result = src.web_transform.tf_data_to_output(
+            self.csv_file,
+            selected_columns=self.tf_config['columns'],
+            index=self.tf_config['index']
+        )
+
+        dummy_np_array = np.array([[220,  11],
+                                   [314,   8],
+                                   [345,   4],
+                                   [378,   7],
+                                   [449,   4]], dtype=int)
+
+        assert list(result.columns) == ['user_id', '/']
+
+        np.testing.assert_array_equal(result.values, dummy_np_array)
