92c8093f580d7110b306aef301ef3e1ccc1f4d87
IanDoarn <baxterhusky3@gmail.com>
2021-11-11 09:40:23 -0500

    More cleanup

--- src/web_transform.py
+++ src/web_transform.py
@@ -8,21 +8,15 @@ from src.util import download_file_from_url, generate_file_paths
 
 def download_csv_files(
         root_url: str,
-        directory: str,
-        create_directory_if_missing: bool = True
+        directory: str
 ) -> List[str]:
     """
     Download a set of CSV files into a directory.
-
-    If a directory is not supplied use the default directory
 
     :param root_url: Root URL where files are stored
     :type root_url: str
     :param directory: Directory to use for files
     :type directory: str
-    :param create_directory_if_missing: Whether or not to make the directory for downloading if
-                                        is it missing
-    :type create_directory_if_missing: bool
     :return: List of files (path/to/file) downloaded
     :rtype: List[str]
     """
@@ -32,10 +26,7 @@ def download_csv_files(
 
     # make sure working directory exists
     if not os.path.isdir(directory):
-        if create_directory_if_missing:
-            os.mkdir(directory)
-        else:
-            raise NotADirectoryError(f'{directory} does not exist')
+        os.mkdir(directory)
 
     for url in file_urls:
         out_file = Path(directory) / url.split('/')[-1]
